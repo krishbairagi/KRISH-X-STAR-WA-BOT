60be23e1a52be57b4a352bc2026f3694fd45b4f0
krishbairagi <krishbairag@gmail.com>
2026-05-30 23:40:19 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// ====================================
+// KRISH WHATSAPP MUSIC BOT
+// CREATED BY KRISH X STAR
+// OWNER USERNAME : @iwanthotpinkpussy
+// ====================================
+
+module.exports = {
+  botName: "KRISH X STAR MUSIC BOT",
+  ownerName: "KRISH X STAR",
+  prefix: "/"
+}
